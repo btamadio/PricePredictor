898599df2687eda3fb4bd043d81ccbe9a8dac7d9
Brian Amadio <brian@Brians-MacBook-Air-2.local>
2017-01-26 10:52:24 -0800

    List 5 most similar rooms in order of price

--- PricePredictor/views.py
+++ PricePredictor/views.py
@@ -11,6 +11,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import psycopg2
+from geopy.distance import vincenty
 
 @app.route('/')
 def index():
@@ -26,6 +27,7 @@ def my_form_post():
     featureList = joblib.load('PricePredictor/static/featureList_v1.pkl')
     pred_price = forest.predict(df[featureList])[0]
     list_price = df['price'][0]
+    loc = (df['lat'][0],df['lon'][0])
     res['pred'] = pred_price
     res['listed'] = list_price
     
@@ -41,11 +43,14 @@ def my_form_post():
     con = None
     con = psycopg2.connect(database = dbname, user = username, host='localhost', password=pswd)
 
-    sql_query = 'SELECT * FROM city_table WHERE (pred_price > '+str(pred_price)+' AND price < '+str(list_price)+');'
+    sql_query = 'SELECT index,pred_price,price,lat,lon FROM city_table;'
     airbnb_df = pd.read_sql_query(sql_query,con)
-    airbnb_df = airbnb_df.sort('price',ascending=1)
-    #airbnb_df[['room_id','price']].head()
-    #res['suggestions'] = [{'room_id':row['room_id'],'price':row['price']} for row in airbnb_df]
+    airbnb_df['loc'] = airbnb_df[['lat','lon']].apply(tuple,axis=1)
+    airbnb_df['distance'] = airbnb_df['loc'].apply( lambda x: vincenty(x,loc).miles )
+    airbnb_df['sim_dist'] = airbnb_df['pred_price'].apply( lambda x: abs(x - pred_price) )
+    airbnb_df = airbnb_df.sort('sim_dist',ascending=1).head(5).sort('price',ascending=1)
+    
+    print(airbnb_df.head())
     res['suggestions'] = airbnb_df.head(5).to_dict('records')
     return render_template("results.html",
        title = 'Home',
