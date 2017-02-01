8dcb28f445691a4b31936efe4bdc88f828ff45d8
Brian Amadio <brian@Brians-MacBook-Air-2.local>
2017-01-31 17:25:30 -0800

    Binary features with feature importance metric

--- PricePredictor/views.py
+++ PricePredictor/views.py
@@ -32,6 +32,8 @@ def my_form_post():
     selected_df = pd.DataFrame(featureDict,index=[int(res['room_id'])])
     selected_df = selected_df[dbList]
     list_price = int(selected_df['price'].iloc[0].strip('$'))
+    #selected_df['price'].apply(lambda x: int(x.strip('$')))
+#    print(selected_df['price'])
     loc = (selected_df['lat'].iloc[0],selected_df['lon'].iloc[0])
     print('Querying database for room info')
     dbname = 'airbnb_db'
@@ -44,16 +46,15 @@ def my_form_post():
     sql_query = 'SELECT * FROM binary_table;'
     full_df = pd.read_sql_query(sql_query,con,index_col='index')
     full_df['loc'] = full_df[['lat','lon']].apply(tuple,axis=1)
-
     print('Calculating distances')
     #calculate distance between our listing and all others
     full_df['distance'] = full_df['loc'].apply( lambda x: vincenty(x,loc).miles )
     full_df = full_df[full_df.distance < max_dist]
-    max_price = 1.1*list_price
-    full_df = full_df[full_df.price < max_price]
+#    max_price = 1.1*list_price
+#    full_df = full_df[full_df.price < max_price]
     full_df = full_df[full_df.index != int(res['room_id'])]
-    if selected_df['person_cap'].values[0] > 1:
-        full_df = full_df[full_df.person_cap > 1]
+#    if selected_df['person_cap'].values[0] > 1:
+#        full_df = full_df[full_df.person_cap > 1]
     print('Calculating feature vectors')
     def getFeatureVec(d):
         return [int(d[i]) for i in featureList]
@@ -75,18 +76,40 @@ def my_form_post():
     selected_df['feature_vec'] = selected_df.apply(getFeatureVec,1)
     print('Calculating similarity distance')
     full_df['sim_dist'] = full_df['feature_vec'].apply(lambda x:sim_dist(x,selected_df['feature_vec'].values[0]))
-#    full_df['sim_dist'] = full_df['feature_vec'].apply(lambda x:cosine(x,selected_df['feature_vec'].values[0]))
-
-    # #list the 5 most similar in order of price
-    full_df = full_df.sort('sim_dist',ascending=1).head(5).sort('price',ascending=1)
+    cancel_dict = {3:'Flexible',4:'Moderate',5:'Strict'}
+    # #list the 10 most similar in order of price
+    full_df = full_df.sort('sim_dist',ascending=1).head(10).sort('price',ascending=1)
     full_df['ind'] = full_df.index
+
     full_df['price'] = full_df['price'].apply( lambda x : '${0:.0f}'.format(x) )
     full_df['distance'] = full_df['distance'].apply( lambda x : '{0:.2f}'.format(x) )
     full_df['person_cap'] = full_df['person_cap'].apply(lambda x: int(x))
-    full_df['cleanliness_rating'] = full_df['cleanliness_rating'].apply(lambda x: int(x))
     full_df['guest_sat'] = full_df['guest_sat'].apply(lambda x: int(x))
     full_df['num_beds'] = full_df['num_beds'].apply(lambda x:int(x))
-    res['suggestions'] = full_df.head(6).to_dict('records')
+    full_df['cable_tv'] = full_df['amen_2'].apply(lambda x:'Yes' if x else 'No')
+    full_df['tv'] = full_df['amen_1'].apply(lambda x:'Yes' if x else 'No')
+    full_df['cancel_policy'] = full_df['cancel_policy'].apply( lambda x: cancel_dict[x])
+    full_df['cleanliness_rating'] = full_df['cleanliness_rating'].apply(lambda x: int(x))
+    full_df['family_friendly'] = full_df['amen_31'].apply(lambda x:'Yes' if x else 'No')
+    full_df['kitchen'] = full_df['amen_8'].apply(lambda x:'Yes' if x else 'No')
+    full_df['parking'] = full_df['amen_9'].apply(lambda x:'Yes' if x else 'No')
+    full_df['internet'] = full_df['amen_3'].apply(lambda x:'Yes' if x else 'No')
+
+#    selected_df['price'] = selected_df['price'].apply( lambda x : '${0:.0f}'.format(x) )
+#    selected_df['distance'] = selected_df['distance'].apply( lambda x : '{0:.2f}'.format(x) )
+    selected_df['person_cap'] = selected_df['person_cap'].apply(lambda x: int(x))
+    selected_df['guest_sat'] = selected_df['guest_sat'].apply(lambda x: int(x))
+    selected_df['num_beds'] = selected_df['num_beds'].apply(lambda x:int(x))
+    selected_df['cable_tv'] = selected_df['amen_2'].apply(lambda x:'Yes' if x else 'No')
+    selected_df['tv'] = selected_df['amen_1'].apply(lambda x:'Yes' if x else 'No')
+    selected_df['cancel_policy'] = selected_df['cancel_policy'].apply( lambda x: cancel_dict[x])
+    selected_df['cleanliness_rating'] = selected_df['cleanliness_rating'].apply(lambda x: int(x))
+    selected_df['family_friendly'] = selected_df['amen_31'].apply(lambda x:'Yes' if x else 'No')
+    selected_df['kitchen'] = selected_df['amen_8'].apply(lambda x:'Yes' if x else 'No')
+    selected_df['parking'] = selected_df['amen_9'].apply(lambda x:'Yes' if x else 'No')
+    selected_df['internet'] = selected_df['amen_3'].apply(lambda x:'Yes' if x else 'No')
+
+    res['suggestions'] = full_df.head(10).to_dict('records')
     res['this_room'] = selected_df.to_dict('records')
 
     return render_template("results.html",
